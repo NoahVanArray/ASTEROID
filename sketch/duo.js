43af68df6b3f6b7e5f1d879465b2a18554ae861b
yoe <johnyohannavarra@gmail.com>
2026-01-28 14:46:59 +0800

    Overwrote Max's for testing (TO FIX).

---
+++ sketch/duo.js
@@ -0,0 +1,10 @@
+
+// this file is for the Duo Gamemode
+
+function setup(){
+
+}
+
+function draw(){
+
+}
---
+++ sketch/pvp.js
@@ -0,0 +1,10 @@
+
+// this file is for the PVP Gamemode
+
+function setup(){
+
+}
+
+function draw(){
+
+}
